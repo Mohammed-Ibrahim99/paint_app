17f25af25be7a9435da92455a441e7ac052a7b2d
Mohammed-Ibrahim99 <moaik19@gmail.com>
2023-03-13 01:47:11 +0000

    Finished tutorial from here https://www.youtube.com/watch?v=Zv5T2C1oKus, need to fix null issue when stopping the use of canvas

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+// All credit goes to:
+// https://www.youtube.com/watch?v=Zv5T2C1oKus
 import 'dart:html';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,16 +21,24 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter'),
+      home: MyHomePage(title: 'Flutter'),
     );
   }
 }
 
+class DrawingArea
+{
+  Offset point;
+  Paint areaPaint;
+
+  DrawingArea({required this.point, required this.areaPaint});
+}
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
+
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -34,6 +46,45 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  List<DrawingArea> points = [];
+  Color selectedColor = Colors.black;
+  double? strokeWidth;
+
+  @override
+  void initState()
+  {
+    super.initState();
+    selectedColor = Colors.black;
+    strokeWidth = 2.0;
+  }
+    void selectColour()
+    {
+      showDialog(context: context,
+          builder: (context) => AlertDialog(
+            title: const Text("Colour Chooser"),
+            content: SingleChildScrollView(
+              child: BlockPicker(
+                pickerColor: selectedColor,
+                onColorChanged: (color)
+                {
+                  this.setState(() {
+                    selectedColor = color;
+                  });
+                },
+              ),
+            ),
+            // actions: <Widget>[
+            //   FlatButton(
+            //     onPressed: ()
+            //         {
+            //           Navigator.of(context).pop();
+            //         },
+            //     child: Text("Close"))
+            // ],
+          ),
+
+      );
+    }
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
@@ -76,16 +127,74 @@ class _MyHomePageState extends State<MyHomePage> {
                     )
                   ]
                   ),
+
+                  /***************************************
+                   * Detecting when user clicks on      *
+                   * canvas to draw                     *
+                   **************************************/
+                  child: GestureDetector
+                  (
+                    //User Tapping on screen
+                    onPanDown: (details)
+                    {
+                      this.setState((){points.add(DrawingArea(point: details.localPosition,
+                          areaPaint: Paint()
+                            ..strokeCap = StrokeCap.round
+                            ..isAntiAlias = true
+                            ..color = selectedColor
+                            ..strokeWidth = strokeWidth!));});
+                    },
+
+                    //User moving on screen
+                    onPanUpdate: (details)
+                    {
+                      this.setState((){points.add(DrawingArea(point: details.localPosition,
+                          areaPaint: Paint()
+                            ..strokeCap = StrokeCap.round
+                            ..isAntiAlias = true
+                            ..color = selectedColor
+                            ..strokeWidth = strokeWidth!));});
+                    },
+
+                    //User removing finger from screen
+                    onPanEnd: (details)
+                    {
+                      this.setState(() {
+                        points.add(null!);
+                      });
+
+                    },
+
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    child: CustomPaint
+                           (
+                              painter: MyCustomPainter(points: points, color: selectedColor, strokeWidth: strokeWidth!),
+                           ),
+                  ),
+                  ),
                 ),
                 const SizedBox(
                   height: 10,
                 ),
+
+                /***************************************
+                 * Space between canvas and buttons    *
+                 **************************************/
+
+                const SizedBox
+                (
+                  height: 20
+                ),
+
                 /***************************************
                  * Buttons at the Bottom of the screen *
                  **************************************/
+
                 Container(
                   width: width * 0.80,
-                  decoration: BoxDecoration(
+                  decoration: const BoxDecoration
+                  (
                     color: Colors.white,
                     borderRadius: BorderRadius.all(Radius.circular(20.0))
                   ),
@@ -93,12 +202,27 @@ class _MyHomePageState extends State<MyHomePage> {
                     children: <Widget>[
                       ButtonBar(
                         children: [
-                          IconButton(onPressed: null,
-                              icon: const Icon(Icons.color_lens)),
-                          IconButton(onPressed: null,
-                              icon: const Icon(Icons.layers_clear)),
-                          IconButton(onPressed: null,
-                              icon: const Icon(Icons.select_all))
+                          IconButton(onPressed: (){
+                            selectColour();
+                          }, icon: Icon(Icons.color_lens, color: selectedColor,)),
+
+                          Expanded(child: Slider(
+                              min: 1.0,
+                              max: 7.0,
+                              activeColor: selectedColor,
+                              value:strokeWidth!, onChanged:(value)
+                          {
+                            this.setState(() {
+                              strokeWidth = value;
+                            });
+                          })),
+
+                          IconButton(onPressed: (){
+                            this.setState(() {
+                              points.clear();
+                            });
+                          }, icon: Icon(Icons.layers_clear)),
+                          
                         ],
                       )
                     ],
@@ -114,4 +238,44 @@ class _MyHomePageState extends State<MyHomePage> {
     )
     );
   }
+}
+
+
+class MyCustomPainter extends CustomPainter
+{
+  List<DrawingArea> points;
+  Color color;
+  double strokeWidth;
+
+  MyCustomPainter({required this.points, required this.color, required this.strokeWidth});
+
+  @override
+  void paint(Canvas canvas, Size size)
+  {
+    Paint background = Paint()..color = Colors.white;
+    Rect rect = Rect.fromLTWH(0, 0, size.width, size.height);
+    canvas.drawRect(rect, background);
+
+
+    for(int x=0; x<points.length-1; x++)
+      {
+        if(points[x] != null && points[x+1] != null)
+          {
+            Paint paint = points[x].areaPaint;
+            canvas.drawLine(points[x].point, points[x + 1].point, paint);
+          }
+        else if (points[x] != null && points[x+1] == null)
+          {
+            Paint paint = points[x].areaPaint;
+            canvas.drawPoints(PointMode.points, [points[x].point], paint);
+          }
+      }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate)
+  {
+    return true;
+  }
+
 }
